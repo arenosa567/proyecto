d071fbf107729aa264657a72a2e52bcd2cde40ea
arenosa567 <arenosa3@gmail.com>
2018-05-31 20:29:22 +0200

    primera modificacion

--- uf1466/copiaseguridadlamp.sh
+++ uf1466/copiaseguridadlamp.sh
@@ -1,24 +1,24 @@
 
 #documentos personales de los usuarios
 function home(){
-  rsync -av /home /home/paco/Documentos/web/git/proyecto/uf1466/backups/
+  rsync -av /home /home/paco/Documentos/backups/
 }
 #archivos y aplicaciones publicados en el servidor http
 function servidorapache(){
     #apache
-   rsync -av /var/www /home/paco/Documentos/web/git/proyecto/uf1466/backups/
+   rsync -av /var/www /home/paco/Documentos/backups/
 }
 function servidornode(){
     #node
     cd /home/paco/Documentos/node-v10.1.0-linux-x64
     rsync -av lib/ /usr/local/lib/
     rsync -av bin/ /usr/local/bin/
-    rsync -av /usr/local/bin/ /home/paco/Documentos/web/git/proyecto/uf1466/backups/node/bin/
-    rsync -av /usr/local/lib/node_modules /home/paco/Documentos/web/git/proyecto/uf1466/backups/node/lib/
+    rsync -av /usr/local/bin/ /home/paco/Documentos/backups/node/bin/
+    rsync -av /usr/local/lib/node_modules /home/paco/Documentos/backups/node/lib/
 } 
 
 function basedatos(){
-    cd /home/paco/Documentos/web/git/proyecto/uf1466/basedato/
+    cd /home/paco/Documentos/backups/basedato/
     #wordpress
     wp=$(mysqldump -u root -psecret wordpress)
 
