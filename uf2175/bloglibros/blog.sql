8270ec2b1d6ffca87bb144ce4ee67618398ab0a0
arenosa567 <arenosa3@gmail.com>
2018-07-23 16:02:13 +0200

    modificado blog.sql

--- uf2175/bloglibros/blog.sql
+++ uf2175/bloglibros/blog.sql
@@ -3,17 +3,30 @@ CREATE DATABASE IF NOT EXISTS blog DEFAULT CHARACTER SET utf8 COLLATE utf8_unico
 USE blog;
 DROP TABLE IF EXISTS `blogs`; 
 
-CREATE TABLE `blogs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `autor` varchar(45) COLLATE utf8_spanish_ci ,
-   editorial varchar(60),	
-  `titulo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-   categoria varchar(40),	
-  `contenido` text,
-  `fecha` DATE,
-   imagen text
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+
+CREATE TABLE IF NOT EXISTS `blogs` (
+  
+  `idblogs` int(11) NOT NULL AUTO_INCREMENT,
+
+  `autor` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+
+  `titulo` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+
+  `editorial` varchar(60) COLLATE utf8_spanish_ci DEFAULT NULL,
+
+  `fecha` varchar(10) COLLATE utf8_spanish_ci DEFAULT NULL,
+
+  `categoria` varchar(40) COLLATE utf8_spanish_ci DEFAULT NULL,
+
+  `contenido` text COLLATE utf8_spanish_ci,
+
+  `imagen` text COLLATE utf8_spanish_ci,
+
+   PRIMARY KEY(idblogs)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+
 
 alter table blogs add editorial varchar(60) after titulo;
 alter table blogs add imgagen text; 
